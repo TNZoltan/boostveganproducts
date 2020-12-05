c06ce6f4a810613a04ce70c7d6dcc91d38caf0a0
FilipZawadka <filip.zawadka@hotmail.com>
2020-12-05 22:07:01 +0100

    crawlerinitial

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,3 +1,17 @@
 const helper = require('./helper')
 
 // USE this: helper.isQualifying
+
+all_links = []
+
+start_link = "https://www.facebook.com/burgerking"
+
+function get_links(start_link){
+    axios.get(start_link).then(res => {
+        console.log(`Checking ${url}`)
+        const html = res.data.toString()
+        const temp_links = zoltan_fun(html).filter(e => !all_links.includes(e))
+        all_links.concat(temp_links)
+        temp_links.forEach(get_links);
+    });
+}
